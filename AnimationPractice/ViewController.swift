cd2e21280e05412ef1917168bcb1bc2ae45c4040
Larisa Marcinevskaya <marcinevskaya93@mail.ru>
2021-04-28 03:45:56 +0300

    sending title for button

--- AnimationPractice/ViewController.swift
+++ AnimationPractice/ViewController.swift
@@ -13,31 +13,28 @@ class ViewController: UIViewController {
     @IBOutlet var infoLabel: UILabel!
     @IBOutlet var animationView: SpringView!
     
-    private var animationStarted = false
-    
     var animations = InfoAnimation.getInfoAnimation()
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-    }
+    private var animationStarted = false
     
 
     @IBAction func runAnimationButton(_ sender: SpringButton) {
-       
+        
         for animation in animations {
+            
             animationView.animation = animation.namesAnimation.randomElement()!
             animationView.curve = animation.nameCurve.randomElement()!
             animationView.force = CGFloat(Float.random(in: 0.2...1.0))
             animationView.duration = CGFloat(Float.random(in: 0.5...2.0))
-            animationView.delay = CGFloat(Float.random(in: 0.5...2.0))
+            animationView.delay = CGFloat(Float.random(in: 0.2...0.5))
         animationView.animate()
             infoLabel.text = " \(animationView.animation)\n \(animationView.curve)\n \(String(format: "%.2f", animationView.force))\n \((String(format: "%.2f",animationView.duration)))\n \((String(format: "%.2f",animationView.delay)))"
             
+            sender.setTitle("\(animationView.animation)", for: .normal)
             
         }
-    }
     
+    }
 
 }
 
